34e4a0c3a9d21cf7d654ec42ef4bb9c66d4fcc98
鈴木翔 <tsubasasuzuki@suzukishounoMacBook-Pro.local>
2020-05-12 22:08:39 +0800

    add name function

---
+++ src/Firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBYpZ741GAzEXJlDgzUl0MHUgkZ3tiagr4",
+  authDomain: "tsubacht.firebaseapp.com",
+  databaseURL: "https://tsubacht.firebaseio.com",
+  projectId: "tsubacht",
+  storageBucket: "tsubacht.appspot.com",
+  messagingSenderId: "221072790718",
+  appId: "1:221072790718:web:793b166a61778263027aef",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
--- src/compornents/pages/Register.js
+++ src/compornents/pages/Register.js
@@ -1,9 +1,42 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import firebase from "../../Firebase";
+import md5 from "md5";
 
 import "../styles/register.css";
 
 class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: null,
+      email: null,
+      password: null,
+      passwordConfirmation: null,
+    };
+  }
+  nameHandler = (e) => {
+    this.setState({ name: e.currentTarget.value });
+  };
+
+  passwordHandler = (e) => {
+    this.setState({ password: e.currentTarget.value });
+  };
+
+  emailHandler = (e) => {
+    this.setState({ email: e.currentTarget.value });
+  };
+
+  passwordConfirmationHandler = (e) => {};
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(this.state.email, this.state.password);
+  };
+
   render() {
     return (
       <div className="container">
@@ -14,43 +47,58 @@ class Register extends Component {
                 <i class="puzzle piece icon"></i>Register for Awesome Chat
               </div>
             </h2>
-            <form className="ui large form">
+            <form className="ui large form" onSubmit={this.handleSubmit}>
               <div className="ui stacked segment">
                 <div className="field">
                   <div className="ui left icon input">
                     <i className="user icon"></i>
-                    <input type="text" name="email" placeholder="Username" />
+                    <input
+                      type="text"
+                      name="name"
+                      placeholder="Username"
+                      onChange={this.nameHandler}
+                    />
                   </div>
                 </div>
                 <div className="field">
                   <div className="ui left icon input">
+                    <i class="envelope outline icon"></i>
+                    <input
+                      type="text"
+                      name="email"
+                      placeholder="Email Address"
+                      onChange={this.emailHandler}
+                    />
+                  </div>
+                </div>
+                <div class="field">
+                  <div class="ui left icon input">
                     <i className="lock icon"></i>
                     <input
                       type="password"
                       name="password"
-                      placeholder="Email Address"
+                      placeholder="Password"
+                      onChange={this.passwordHandler}
                     />
                   </div>
                 </div>
                 <div class="field">
                   <div class="ui left icon input">
-                    <i class="user icon"></i>
-                    <input type="text" name="email" placeholder="Password" />
-                  </div>
-                </div>
-                <div class="field">
-                  <div class="ui left icon input">
-                    <i class="user icon"></i>
+                    <i class="redo icon"></i>
                     <input
-                      type="text"
-                      name="email"
+                      type="password"
+                      name="password"
                       placeholder="Password Confirmation"
+                      onChange={this.passwordConfirmationHandler}
                     />
                   </div>
                 </div>
-                <div className="ui fluid large teal submit button register_button">
+                <button
+                  type="submit"
+                  className="ui fluid large teal submit button register_button"
+                >
                   Sign Up
-                </div>
+                </button>
               </div>
 
               <div className="ui error message"></div>
